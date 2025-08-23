1fc678ad55f453de98e301bb15a4a1ac02aa2d10
Aleksandrs Apilats <aleksandrs.apilats@oag.com>
2025-08-23 11:38:08 +0100

    replaced Printf with Sprintf

--- 01-investment/investment_calculator.go
+++ 01-investment/investment_calculator.go
@@ -27,8 +27,14 @@ func main() {
 	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
 	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
 
+	//Sprintf allows you to format and store a string
+
+	formattedFV := fmt.Sprintf("Future Value: %.2f\n", futureValue )
+	formattedFRV := fmt.Sprintf("Future Real Value %.2f\n", futureRealValue)
+	fmt.Print(formattedFV, formattedFRV)
 	// Output Information
 	// fmt.Println("Future Value:", futureValue)
 	// fmt.Println("Future Real Value:", futureRealValue)
-	fmt.Printf("Future Value: %.2f\nFuture Real Value: %.2f", futureValue, futureRealValue)
+	// Printf allows you to format and print a string
+	//fmt.Printf("Future Value: %.2f\nFuture Real Value: %.2f", futureValue, futureRealValue)
 }
