da39c49477850a261826c0bf486206db9c579be9
Aleksandrs Apilats <aleksandrs.apilats@oag.com>
2025-08-23 10:22:24 +0100

    Initial Commit

---
+++ 02-profit/profit_calculator.go
@@ -0,0 +1,27 @@
+// Profit Calculator
+package main
+
+import "fmt"
+
+func main() {
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+
+	fmt.Print("Revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Tax Rate: ")
+	fmt.Scan(&taxRate)
+
+	earnings_before_tax := revenue - expenses
+	profit := earnings_before_tax - (earnings_before_tax / taxRate)
+	ratio := earnings_before_tax / profit
+
+	fmt.Println("Earnings Before Tax:", earnings_before_tax)
+	fmt.Println("Profit:", profit)
+	fmt.Print("Ratio: ", ratio)
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+## Table of Contents  
+- [Introduction](#introduction)  
+
+## Introduction
+
+The purpose of this project is to learn, practice follow along Go - The Complete Guide by Maximilian Schwarzmüller.
